7a793bdf753a5de97a583ec45f09e8844f358d76
Claire-GetHub <claire@execor.com>
2024-12-02 13:47:50 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,34 @@
 from tkinter import *
 
-# create root window
-root = Tk()
+def main3():
+    root = Tk()
 
-# root window title and dimension
-root.title("OpenSteam")
+    root.geometry("500x500")
+ 
+# Specify Grid
+    Grid.rowconfigure(root,0,weight=1)
+    Grid.columnconfigure(root,0,weight=1)
+    
+    Grid.rowconfigure(root,1,weight=5)
 
-root.geometry("300x300")
-options = ["game1", "game2"]
-optionBox = Listbox(root, height=10, width= 15)
-#for future
-#root.attributes('-fullscreen',True)
+    label = Label(root, text='Games', font= ("Arial", 25))
 
-root.mainloop()
+    options = ["game1", "game2"]
+    optionBox = Listbox(root, font= ("Arial", 25))
+    for option in options:
+        optionBox.insert(options.index(option) + 1, option)
+    
+    button = Button(root, text= "continue", font= ("Arial", 25))
 
+    label.grid(row=0,column=0,sticky="NSEW")      
+    optionBox.grid(row=1,column=0,sticky="NSEW")
+    button.grid(row=2,column=0,sticky="NSEW")
+
+
+    root.mainloop()
+
+
+
+
+if __name__ == "__main__":
+    main3()
