4277db3fd0f047465852d8a47bf22a31a9420edc
Claire-GetHub <claire@execor.com>
2024-12-13 13:36:54 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 from tkinter import *
 import subprocess
 
-
 def main():
     root = Tk()
 
@@ -22,10 +21,18 @@ def main():
 
     # steam, epic games, battlenet, riot
     #all games you can choose
-        # "gameName" : games application id 
-    steam = {"wolfQuest":431180, "Terraria": 105600}
-    epicGames = {}
-    battleNet = {}
+        # "gameName" : *games url* (found by right clicking application > properties) 
+    steam = {
+        "wolfQuest": "steam://rungameid/431180",
+        "Terraria": "steam://rungameid/105600"
+        }
+    epicGames = {
+        "Rocket League": '"com.epicgames.launcher://apps/9773aa1aa54f4f7b80e44bef04986cea%3A530145df28a24424923f5828cc9031a1%3ASugar?action=launch&silent=true"'
+    }
+    #battle net game id is found diffrently.
+    battleNet = {
+        "Overwatch": '"C:\program files (x86)\overwatch\_retail_\overwatch.exe"'
+    }
     riot = {}
 
     options = [steam, epicGames, battleNet, riot]
@@ -36,20 +43,29 @@ def main():
     optionBox = Listbox(root, font= ("Arial", 25))
 
     #adding all the options to the listbox
-    for platform in options:
-        index = 0
+    index = 0
+    for platform in options:        
         for option in platform:
             optionBox.insert(index, option)
             index += 1
 
-
-    #what runs when the button is clicked
+    #has to be here because it uses option box and all the platforms lists
     def runGame():
         for i in optionBox.curselection():
             game = optionBox.get(i)   
 
-        #if game in steam ect.
-        subprocess.run(f"start steam://run/{options[game]}", shell=True)
+
+        if game in steam.keys():
+            platform = steam
+        elif game in epicGames.keys():
+            platform = epicGames
+        elif game in battleNet.keys():
+            platform = battleNet
+        elif game in riot:
+            platform = riot
+
+        #actual code to open game
+        subprocess.run(fr"start {platform[game]}", shell=True)
 
     #the countinue button
         #root, cant be changed how its connected to the window
@@ -68,8 +84,13 @@ def main():
 
     root.mainloop()
 
-
+    #what runs when the button is clicked
 
 
 if __name__ == "__main__":
-    main()
+    pass #main()
+
+game = '"com.epicgames.launcher://apps/9773aa1aa54f4f7b80e44bef04986cea%3A530145df28a24424923f5828cc9031a1%3ASugar?action=launch&silent=true"'
+#subprocess.Popen(["explorer", game], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+
+subprocess.run('start "com.epicgames.launcher://apps/9773aa1aa54f4f7b80e44bef04986cea%3A530145df28a24424923f5828cc9031a1%3ASugar?action=launch&silent=true"', shell=True)
